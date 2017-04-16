213f1d1953429bfb3a3f73f0039019ee4a09353c
John Dodds <jdodds842@gmail.com>
2017-04-16 13:06:45 +0100

    Rename quick-packages.sh to quick-packages1.sh

---
+++ scripts/quick-packages1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt-get update && sudo apt-get -y upgrade
+sudo apt-get install -y git python python-dev python-software-properties python-numpy python-pip nodejs-legacy npm watchdog strace tcpdump screen acpid vim locate jq && \
+sudo pip install -U openaps && \
+sudo pip install -U openaps-contrib && \
+sudo openaps-install-udev-rules && \
+sudo activate-global-python-argcomplete && \
+sudo npm install -g json oref0 && \
+echo openaps installed && \
+openaps --version
